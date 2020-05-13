49052bf8a963d67707cc502a74bceb389ef051a1
Anton <diamonddiver99@gmail.com>
2020-05-13 17:38:38 +0300

    Updates

---
+++ precache-manifest.223bc3ee5d70efb8d2351eda3a151816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff255d97fac033c3976ba8a2c844930",
+    "url": "/EventBizPro-react-app/index.html"
+  },
+  {
+    "revision": "f6d7bd9cbcddc181873b",
+    "url": "/EventBizPro-react-app/static/css/main.34847d0b.chunk.css"
+  },
+  {
+    "revision": "a641630b6622785079ae",
+    "url": "/EventBizPro-react-app/static/js/2.4975f9db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/EventBizPro-react-app/static/js/2.4975f9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d7bd9cbcddc181873b",
+    "url": "/EventBizPro-react-app/static/js/main.8ba1664c.chunk.js"
+  },
+  {
+    "revision": "95cb36f0246bf0b999c1",
+    "url": "/EventBizPro-react-app/static/js/runtime-main.0be9c540.js"
+  }
+]);
